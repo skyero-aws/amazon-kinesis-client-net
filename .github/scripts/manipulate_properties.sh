ecc82a50df738a175e37c6dec11081ce488c0323
skye rogers <skyero@amazon.com>
2025-07-01 11:09:40 -0700

    add -p and -execute to executable name

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -12,7 +12,7 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
   sed -i "" "s/kclnetsample/$STREAM_NAME/g" SampleConsumer/kcl.properties
   sed -i "" "s/DotNetKinesisSample/$APP_NAME/g" SampleConsumer/kcl.properties
   sed -i "" 's/us-east-5/us-east-1/g' SampleConsumer/kcl.properties
-  sed -i "" "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/Bootstrap.dll|g" SampleConsumer/kcl.properties
+  sed -i "" "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/Bootstrap.dll --properties kcl.properties --execute|g" SampleConsumer/kcl.properties
   grep -v "idleTimeBetweenReadsInMillis" SampleConsumer/kcl.properties > SampleConsumer/temp.properties
   echo "idleTimeBetweenReadsInMillis = 250" >> SampleConsumer/temp.properties
   mv SampleConsumer/temp.properties SampleConsumer/kcl.properties
@@ -21,14 +21,14 @@ elif [[ "$RUNNER_OS" == "Linux" ]]; then
   sed -i "s/kclnetsample/$STREAM_NAME/g" SampleConsumer/kcl.properties
   sed -i "s/DotNetKinesisSample/$APP_NAME/g" SampleConsumer/kcl.properties
   sed -i 's/us-east-5/us-east-1/g' SampleConsumer/kcl.properties
-  sed -i "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/Bootstrap.dll|g" SampleConsumer/kcl.properties
+  sed -i "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/Bootstrap.dll --properties kcl.properties --execute|g" SampleConsumer/kcl.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" SampleConsumer/kcl.properties
   sed -i "51s/kclnetsample/$STREAM_NAME/g" SampleProducer/SampleProducer.cs
 elif [[ "$RUNNER_OS" == "Windows" ]]; then
   sed -i "s/kclnetsample/$STREAM_NAME/g" SampleConsumer/kcl.properties
   sed -i "s/DotNetKinesisSample/$APP_NAME/g" SampleConsumer/kcl.properties
   sed -i 's/us-east-5/us-east-1/g' SampleConsumer/kcl.properties
-  sed -i "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/Bootstrap.dll|g" SampleConsumer/kcl.properties
+  sed -i "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/Bootstrap.dll --properties kcl.properties --execute|g" SampleConsumer/kcl.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" SampleConsumer/kcl.properties
   sed -i "51s/kclnetsample/$STREAM_NAME/g" SampleProducer/SampleProducer.cs
 else
